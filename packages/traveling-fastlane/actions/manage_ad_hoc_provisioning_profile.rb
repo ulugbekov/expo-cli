0a6870d96e3b112518336a7ef730165c443c5b97
Quinlan Jung <quin@expo.io>
2019-05-06 08:41:42 -0700

    wip

--- packages/traveling-fastlane/actions/manage_ad_hoc_provisioning_profile.rb
+++ packages/traveling-fastlane/actions/manage_ad_hoc_provisioning_profile.rb
@@ -6,11 +6,6 @@ require_relative 'funcs'
 
 ENV['SPACESHIP_AVOID_XCODE_API'] = '1'
 
-$teamId, $udidsString, $bundleId, $certSerialNumber = ARGV
-$udids = $udidsString.split(',')
-
-$result = nil
-
 def find_dist_cert(serialNumber, isEnterprise)
   certs = if isEnterprise == 'true'
     Spaceship::Portal.certificate.in_house.all
@@ -25,6 +20,12 @@ def find_dist_cert(serialNumber, isEnterprise)
       c.raw_data['serialNum'] == $certSerialNumber
     end
   end
+end
+
+def get_registered_udids()
+  all_iphones = Spaceship.device.all_iphones
+  already_added = all_iphones.select { |d| d.enabled? and udids.include?(d.udid) }
+  already_added.map { |i| i.udid }
 end
 
 def register_missing_devices(udids)
@@ -42,32 +43,103 @@ def register_missing_devices(udids)
   devices
 end
 
-def find_profile_by_bundle_id(bundle_id)
-  expo_profiles = Spaceship::Portal.provisioning_profile.ad_hoc.find_by_bundle_id(bundle_id: bundle_id)
-  expo_profiles = expo_profiles.select{ |profile|
-    profile.name.start_with?('*[expo]') and profile.expires.to_datetime > DateTime.now
+def list()
+  profiles = find_profile_by_bundle_id($bundleId)
+  profiles.map{ |profile|
+    {
+      id: profile.id,
+      name: profile.name
+      expires: profile.expires.to_time.to_i,
+      app: profile.app.name,
+      certificates: profile.certificates.map{ |cert|
+        {
+          serialNumber: cert.raw_data['serialNumber'],
+        }
+      }
+    }
   }
+end
+
+def find_profile_by_id(id)
+  profiles = find_profile_by_bundle_id($bundleId)
+  profiles.select{ |profile| profile.id == id }.last
+end
+
+def find_profile_by_bundle_id(bundle_id)
+  Spaceship::Portal.provisioning_profile.ad_hoc.find_by_bundle_id(bundle_id: bundle_id)
+end
+
+def create(dist_cert)
+  devices = get_registered_udids()
+  new_profile = Spaceship::Portal.provisioning_profile.ad_hoc.create!(
+    bundle_id: $bundleId,
+    certificate: dist_cert,
+    devices: devices
+  )
+  profile = download_provisioning_profile(new_profile)
+  {
+    provisioningProfileId: profile[:id],
+    provisioningProfile: profile[:content],
+  }
+end
+
+def revoke(ids)
+  profiles = find_profile_by_bundle_id($bundleId)
+
   # find profiles associated with our development cert
-  expo_profiles_with_cert = expo_profiles.select{ |profile|
-    profile.certificates.any?{|cert| cert.raw_data['serialNumber'] == $certSerialNumber} # this api only returns field 'serialNumber', but not 'serialNum'
+  profiles_to_delete = profiles.select{ |profile|
+    ids.any?{|id| id == profile.id}
   }
 
-  if !expo_profiles_with_cert.empty?
-    # there is an expo managed profile with our desired certificate
-    # return the profile that will be valid for the longest duration
-    expo_profiles_with_cert.sort_by{|profile| profile.expires}.last  
-  elsif !expo_profiles.empty? 
-    # there is an expo managed profile, but it doesnt have our desired certificate
-    # append the certificate and update the profile 
-    dist_cert = find_dist_cert($certSerialNumber, in_house?($teamId))
-    profile = expo_profiles.sort_by{|profile| profile.expires}.last
-    profile.certificates.push(dist_cert)
-    profile.update!
-  else
-     # there is no valid provisioning profile available
-    nil 
+  profiles_to_delete.each { |profile| profile.delete! }
+end
+
+def add_dist_cert_to_profile(profile_id, cert_serial_number)
+  profile = find_profile_by_dev_portal_id(profile_id)
+  if profile == nil
+    # If the adhoc profile doesn't exist, the user must have deleted it, we can't do anything here :(
+    raise RuntimeError, `Unable to find adhoc profile with id #{profile_id}`
   end
 
+  if profile.certificates.any?{|cert| cert.raw_data['serialNumber'] == cert_serial_number}
+    profile # profile is already associated with cert
+  else
+    # append the certificate and update the profile 
+    dist_cert = find_dist_cert(cert_serial_number, in_house?($teamId))
+    profile.certificates.push(dist_cert)
+    profile.update!
+  end
+end
+
+def add_udids_to_profile(profile_id, udids)
+  # Then we register all missing devices on the Apple Developer Portal. They are identified by UDIDs.
+  devices = register_missing_devices(udids)
+
+  # Then we try to find an already existing provisioning profile for the App ID.
+  existing_profile = find_profile_by_id(profile_id)
+
+  if existing_profile == nil
+    raise RuntimeError, `Unable to find adhoc profile with id #{profile_id}`
+  elsif !existing_profile.valid?
+    raise RuntimeError, `Adhoc profile with id #{profile_id} is invalid`
+  end
+
+  # We need to verify whether the existing profile includes all user's devices.
+  device_udids_in_profile = Set.new(existing_profile.devices.map { |d| d.udid })
+  all_device_udids = Set.new(udids)
+  if device_udids_in_profile == all_device_udids
+    { didUpdateProfile: false }
+  else
+    # We need to add new devices to the list and create a new provisioning profile.
+    existing_profile.devices = devices
+    updated_profile = existing_profile.update!
+    profile = download_provisioning_profile(updated_profile)
+    { 
+      didUpdateProfile: true,         
+      provisioningProfileId: profile[:id],
+      provisioningProfile: profile[:content], 
+    }
+  end
 end
 
 def download_provisioning_profile(profile)
@@ -83,63 +155,65 @@ def in_house?(team_id)
   team['type'] === 'In-House'
 end
 
+$teamId, $bundleId, $action, *$actionArgs  = ARGV
+$result = nil
+
 with_captured_output{
   begin
-    # Spaceship::Portal doesn't seem to have a method for initializing portal client
-    # without supplying Apple ID username/password (or i didn't find one). Fortunately,
-    # we can pass here whatever we like, as long as we set FASTLANE_SESSION env variable.
-    Spaceship::Portal.login('fake-login', 'fake-password')
-    Spaceship::Portal.client.team_id = $teamId
-
-    # Then we register all missing devices on the Apple Developer Portal. They are identified by UDIDs.
-    devices = register_missing_devices($udids)
-
-    # Then we try to find an already existing provisioning profile for the App ID.
-    existing_profile = find_profile_by_bundle_id($bundleId)
-    if existing_profile
-      # We need to verify whether the existing profile includes all user's devices.
-      device_udids_in_profile = Set.new(existing_profile.devices.map { |d| d.udid })
-      all_device_udids = Set.new($udids)
-      if device_udids_in_profile == all_device_udids and existing_profile.valid?
-        $result = { result: 'success' }
-      else
-        # We need to add new devices to the list and create a new provisioning profile.
-        existing_profile.devices = devices
-        existing_profile.update!
-
-        updated_profile = find_profile_by_bundle_id($bundleId)
-        profile = download_provisioning_profile(updated_profile)
-        $result = {
-          result: 'success',
-          provisioningProfileId: profile[:id],
-          provisioningProfile: profile[:content],
-        }
-      end
+    if ENV['APPLE_ID'] && ENV['APPLE_PASSWORD']
+      Spaceship::Portal.login(ENV['APPLE_ID'], ENV['APPLE_PASSWORD'])
+    elsif ENV['FASTLANE_SESSION']
+      # Spaceship::Portal doesn't seem to have a method for initializing portal client
+      # without supplying Apple ID username/password (or i didn't find one). Fortunately,
+      # we can pass here whatever we like, as long as we set FASTLANE_SESSION env variable.
+      Spaceship::Portal.login('fake-login', 'fake-password')
     else
-      # We need to find user's distribution certificate to make a provisioning profile for it.
+      raise ArgumentError, 'Must pass in an Apple Session or Apple login/password'
+    end
+    Spaceship::Portal.client.team_id = $teamId
+    
+    if $action == 'create'
+      $certSerialNumber, * = $actionArgs
       dist_cert = find_dist_cert($certSerialNumber, in_house?($teamId))
-
       if dist_cert == nil
         # If the distribution certificate doesn't exist, the user must have deleted it, we can't do anything here :(
-        $result = {
-          result: 'failure',
-          reason: 'No distribution certificate available to make provisioning profile against',
-        }
-      else
-        # If the provisioning profile for the App ID doesn't exist, we just need to create a new one!
-        new_profile = Spaceship::Portal.provisioning_profile.ad_hoc.create!(
-          name: "*[expo] #{$bundleId} AdHoc", # apple drops [ if its the first char (!!)
-          bundle_id: $bundleId,
-          certificate: dist_cert,
-          devices: devices
-        )
-        profile = download_provisioning_profile(new_profile)
-        $result = {
-          result: 'success',
-          provisioningProfileId: profile[:id],
-          provisioningProfile: profile[:content],
-        }
+        raise RuntimeError, `Unable to find distribution certificate with serial number #{$certSerialNumber}`
       end
+      profile = create(dist_cert)
+      $result = { result: 'success', **profile }
+    elsif $action = 'download'
+      $profileId, * = $actionArgs
+      existing_profile = find_profile_by_id($profileId)
+      profile = = download_provisioning_profile(existing_profile)
+      $result = {
+        result: 'success',
+        provisioningProfileId: profile[:id],
+        provisioningProfile: profile[:content],
+      }
+    elsif $action == 'list'
+      $result = {
+        result: 'success',
+        profiles: list(),
+      }
+    elsif $action == 'revoke'
+      $profileIdsString, * = $actionArgs
+      $profileIds = $profileIdsString.split(',')
+      revoke($profileIds)
+      $result = { result: 'success' }
+    elsif $action == 'add-dist-cert'
+      $profileId, $certSerialNumber = $actionArgs
+      add_dist_cert_to_profile($profileId, $certSerialNumber)
+      $result = { result: 'success' }
+    elsif $action  == 'add-udid'
+      $profileId, $udidsString = $actionArgs
+      $udids = $udidsString.split(',')
+      update_result = add_udids_to_profile($profileId, $udids)
+      $result = {
+        result: 'success',
+        **update_result,
+      }
+    else 
+      raise ArgumentError, `Invalid action #{$action}`
     end
   rescue Spaceship::Client::UnexpectedResponse => e
     $result = {
@@ -156,7 +230,7 @@ with_captured_output{
   rescue Exception => e
     $result = {
       result: 'failure',
-      reason: 'Unknown reason',
+      reason: e.message || 'Unknown reason',
       rawDump: dump_error(e)
     }
   end
